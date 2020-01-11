86b57b85b0c08c6bc8cb830e51aa3d70db80575d
Neil Dobson <ndobson@barnardos.org.au>
2020-01-11 18:20:24 +1100

    Add samples tests and redis channel subscriber (log viewer)

---
+++ src/Examples/SampleTests/Program.cs
@@ -0,0 +1,12 @@
+using NUnitSelfRunner;
+
+namespace SampleTests
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Tests.Run(args);
+        }
+    }
+}
